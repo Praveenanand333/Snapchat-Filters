d185d199b731d102b4d21e142b2ab36e56db01f6
Kabil01 <118066026+Kabil01@users.noreply.github.com>
2023-06-21 13:11:43 +0530

    Add files via upload

---
+++ oil-paint.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import cv2
+
+def apply_filter(frame):
+    # Apply oil paint effect to the frame
+    oil_painted_frame = cv2.xphoto.oilPainting(frame, 7, 1)
+
+    return oil_painted_frame
+
+def filter():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read frame from camera
+        ret, frame = cap.read()
+
+        if ret:
+            # Apply filter
+            filtered_frame = apply_filter(frame)
+
+            # Display the filtered frame
+            cv2.imshow('Filtered Feed', filtered_frame)
+
+        # Check for 'q' key press to exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the camera and destroy windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+filter()
