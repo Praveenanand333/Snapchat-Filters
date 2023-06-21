24eff667d83b2109fe073c3257d1bb74a78cf82a
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-06-21 22:56:25 +0530

    create_bright

---
+++ brightener.py
@@ -0,0 +1,59 @@
+import cv2
+import itertools
+import numpy as np
+from time import time
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+
+def adjust_brightness(frame, brightness):
+    # Convert the frame to the HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Split the channels
+    h, s, v = cv2.split(hsv)
+    
+    # Adjust the value (brightness) channel
+    v = cv2.add(v, brightness)
+    
+    # Limit the pixel values to the valid range [0, 255]
+    #v = cv2.clip(v, 0, 255)
+    
+    # Merge the channels back together
+    hsv = cv2.merge((h, s, v))
+    
+    # Convert the frame back to the BGR color space
+    adjusted_frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    
+    return adjusted_frame
+
+def apply_brighten():
+    
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read frame from camera
+        ret, frame = cap.read()
+
+        if ret:
+            # Apply filter
+            filtered_frame = adjust_brightness(frame,50)
+
+            # Display the filtered frame
+            cv2.imshow('BRIGHTENED FEED', filtered_frame)
+
+        # Check for 'q' key press to exit
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the camera and destroy windows
+    cap.release()
+    cv2.destroyAllWindows()
+apply_brighten()
+
+
+
+   
+
