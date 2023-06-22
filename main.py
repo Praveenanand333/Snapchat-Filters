97326861bba16c82de423d5a7f4e75ff51ba47e1
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-06-22 10:46:53 +0530

    create-main

--- main.py
+++ main.py
@@ -5,4 +5,24 @@ from time import time
 import mediapipe as mp
 import matplotlib.pyplot as plt
 import datetime
+import tkinter as tk
+
+import glass as g
+import mustache as m 
+import oil_paint as o 
+import dog_filter as d 
+import fire_eyes as f 
+import brightener as b 
+import bw 
+import blur as bl 
+
+#g.apply_glass()
+#m.apply_mustache()
+#o.apply_filter()
+#d.apply_dogfilter()
+#f.apply_fire_eyes()
+#b.adjust_brightness()
+#bw.apply_bw()
+#bl.apply_blur()
+
 
